27ca3ed013d1579de14de9a8391af6a6b474bae5
wildanrizkii <wildanrizki9560@gmail.com>
2025-08-30 12:45:24 +0700

    Handle API Again

--- src/app/api/participant/[id]/route.ts
+++ src/app/api/participant/[id]/route.ts
@@ -49,7 +49,7 @@ export async function GET(
       return NextResponse.json({ error: "Forbidden" }, { status: 403 });
     }
 
-    const participant = await db.participant.findUnique({
+    const participant = await db.participant.findFirst({
       where: { id },
       include: {
         disc: true,
